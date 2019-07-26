3520387ca47fa9dc0d3b1077659e5e72859da86d
Will Hodgson <will.hodgson@current-rms.com>
2019-07-26 09:27:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,6 +23,8 @@ def assign_rooms(names)
 end
 
 def printer(names)
-  puts batch_badge_creator(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
   puts assign_rooms(names)
 end
